bf8eed37b9e7266d6b72c5b9a67abc740b5e9492
Anil Kulkarni <anil@terminal.space>
2023-11-24 18:35:23 -0800

    Add snapper commands

---
+++ tests/test_snapper.py
@@ -0,0 +1,145 @@
+import json
+import subprocess
+from unittest.mock import MagicMock, Mock, call, patch
+
+import pytest
+
+from dfu.snapshots.snapper import Snapper
+from dfu.snapshots.snapper_diff import FileChangeAction, SnapperDiff
+
+
+@pytest.fixture
+def snapper_instance():
+    return Snapper('test')
+
+
+@patch('subprocess.run')
+def test_get_mountpoint_success(mock_run):
+    mock_run.return_value = Mock(stdout='{"SUBVOLUME": "/test"}')
+    snapper = Snapper('test')
+    assert snapper.get_mountpoint() == '/test'
+
+
+@patch('subprocess.run')
+def test_get_mountpoint_no_subvolume(mock_run):
+    mock_run.return_value = Mock(stdout='{}')
+    snapper = Snapper('test')
+    with pytest.raises(KeyError):
+        snapper.get_mountpoint()
+
+
+@patch('subprocess.run')
+def test_get_mountpoint_invalid_json(mock_run):
+    mock_run.return_value = Mock(stdout='{')
+    snapper = Snapper('test')
+    with pytest.raises(json.JSONDecodeError):
+        snapper.get_mountpoint()
+
+
+@patch('subprocess.run')
+def test_get_mountpoint_subprocess_error(mock_run):
+    mock_run.side_effect = subprocess.CalledProcessError(1, 'cmd')
+    snapper = Snapper('test')
+    with pytest.raises(subprocess.CalledProcessError):
+        snapper.get_mountpoint()
+
+
+@patch('subprocess.run')
+def test_create_pre_snapshot_success(mock_run):
+    mock_run.return_value = Mock(stdout='1\n')
+    snapper = Snapper('test')
+    assert snapper.create_pre_snapshot('description') == 1
+
+
+@patch('subprocess.run')
+def test_create_pre_snapshot_subprocess_error(mock_run):
+    mock_run.side_effect = subprocess.CalledProcessError(1, 'cmd')
+    snapper = Snapper('test')
+    with pytest.raises(subprocess.CalledProcessError):
+        snapper.create_pre_snapshot('description')
+
+
+@patch('subprocess.run')
+def test_create_pre_snapshot_invalid_output(mock_run):
+    mock_run.return_value = Mock(stdout='not a number\n')
+    snapper = Snapper('test')
+    with pytest.raises(ValueError):
+        snapper.create_pre_snapshot('description')
+
+
+@patch('subprocess.run')
+def test_create_post_snapshot_success(mock_run):
+    mock_run.return_value = Mock(stdout='2\n')
+    snapper = Snapper('test')
+    assert snapper.create_post_snapshot(1, 'description') == 2
+
+
+@patch('subprocess.run')
+def test_create_post_snapshot_subprocess_error(mock_run):
+    mock_run.side_effect = subprocess.CalledProcessError(1, 'cmd')
+    snapper = Snapper('test')
+    with pytest.raises(subprocess.CalledProcessError):
+        snapper.create_post_snapshot(1, 'description')
+
+
+@patch('subprocess.run')
+def test_create_post_snapshot_invalid_output(mock_run):
+    mock_run.return_value = Mock(stdout='not a number\n')
+    snapper = Snapper('test')
+    with pytest.raises(ValueError):
+        snapper.create_post_snapshot(1, 'description')
+
+
+@patch('subprocess.run')
+def test_get_delta_success(mock_run, snapper_instance):
+    mock_run.return_value = MagicMock(stdout='+..... test\n')
+    expected_result = [SnapperDiff(path='test', action=FileChangeAction.created, permissions_changed=False)]
+    result = snapper_instance.get_delta(1, 2)
+    assert result == expected_result
+
+
+@patch('subprocess.run')
+def test_get_delta_multiple_lines(mock_run, snapper_instance):
+    mock_run.return_value = MagicMock(stdout='+..... test1\n-..... test2\n')
+    expected_result = [
+        SnapperDiff(path='test1', action=FileChangeAction.created, permissions_changed=False),
+        SnapperDiff(path='test2', action=FileChangeAction.deleted, permissions_changed=False),
+    ]
+    result = snapper_instance.get_delta(1, 2)
+    assert result == expected_result
+
+
+@patch('subprocess.run')
+def test_get_delta_empty_string(mock_run, snapper_instance):
+    mock_run.return_value = MagicMock(stdout='')
+    expected_result = []
+    result = snapper_instance.get_delta(1, 2)
+    assert result == expected_result
+
+
+@patch('subprocess.run')
+def test_get_delta_different_actions_and_permissions(mock_run, snapper_instance):
+    mock_run.return_value = MagicMock(stdout='+..... test1\n-..... test2\nc..... test3\n')
+    expected_result = [
+        SnapperDiff(path='test1', action=FileChangeAction.created, permissions_changed=False),
+        SnapperDiff(path='test2', action=FileChangeAction.deleted, permissions_changed=False),
+        SnapperDiff(path='test3', action=FileChangeAction.modified, permissions_changed=False),
+    ]
+    result = snapper_instance.get_delta(1, 2)
+    assert result == expected_result
+
+
+def test_mount_snapshot():
+    with patch('subprocess.run') as mock_subprocess_run:
+        mock_subprocess_run.return_value = MagicMock(stdout='{"SUBVOLUME": "/path/to/mountpoint"}')
+        snapper_instance = Snapper('home')
+        snapshot_id = 1
+        directory = '/mnt'
+
+        snapper_instance.mount_snapshot(snapshot_id, directory)
+
+        expected_calls = [
+            call(['snapper', '-c', snapper_instance.snapper_name, '--jsonout', 'get-config'], capture_output=True),
+            call(['mount', '--bind', '/path/to/mountpoint/.snapshots/1/snapshot', directory], check=True),
+        ]
+        mock_subprocess_run.assert_has_calls(expected_calls, any_order=False)
