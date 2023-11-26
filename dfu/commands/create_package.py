286c9d08dc9404452d14ea2af0c61aab012c66e2
Anil Kulkarni <anil@terminal.space>
2023-11-25 22:04:31 -0800

    fix typing errors

--- dfu/commands/create_package.py
+++ dfu/commands/create_package.py
@@ -7,8 +7,7 @@ from dfu.package.version_number import get_version_number
 
 def create_package(config: Config, name: str, description: str | None = None) -> Path:
     package = PackageConfig(name=name, description=description, state=State.new)
-    version_number = get_version_number(config)
-    version_number = str(version_number).zfill(5)
+    version_number = str(get_version_number(config)).zfill(5)
     package_name = Path(config.package_dir) / f"{version_number}_{name}" / "dfu_config.json"
     package_name.mkdir(parents=True, exist_ok=False)
     package.write(package_name)
