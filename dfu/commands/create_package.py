661caf88fc1837c958a151e4ac99e0426561a4e0
Anil Kulkarni <anil@terminal.space>
2023-12-03 19:41:55 -0800

    fix init command

--- dfu/commands/create_package.py
+++ dfu/commands/create_package.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
 from dfu.config import Config
-from dfu.package.package_config import PackageConfig, State
+from dfu.package.package_config import PackageConfig
 from dfu.package.version_number import get_version_number
 
 
 def create_package(config: Config, name: str, description: str | None = None) -> Path:
-    package = PackageConfig(name=name, description=description, state=State.new)
+    package = PackageConfig(name=name, description=description)
     version_number = str(get_version_number(config)).zfill(5)
     package_name = Path(config.package_dir) / f"{version_number}_{name}" / "dfu_config.json"
     package_name.mkdir(parents=True, exist_ok=False)
