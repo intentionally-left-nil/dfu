5fa0ad2677a36b73c75ae3f68b18b3ebdc681312
Anil Kulkarni <anil@terminal.space>
2023-12-04 00:01:25 -0800

    Add diff command to update the packages installed

--- dfu/cli.py
+++ dfu/cli.py
@@ -2,6 +2,7 @@ import click
 
 from dfu.commands.create_package import create_package
 from dfu.commands.create_snapshot import create_post_snapshot, create_pre_snapshot
+from dfu.commands.diff import diff_snapshot
 from dfu.commands.load_config import load_config
 from dfu.commands.load_package_config import get_package_path
 
@@ -45,6 +46,14 @@ def end(package_name: str):
     config = load_config()
     package_path = get_package_path(config, package_name)
     create_post_snapshot(package_path)
+
+
+@main.command()
+@click.argument("package_name")
+def diff(package_name: str):
+    config = load_config()
+    package_path = get_package_path(config, package_name)
+    diff_snapshot(package_path)
 
 
 if __name__ == "__main__":
