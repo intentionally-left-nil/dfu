5dfb8bdf5d2a4e8b23199203331ad56bd444525f
Anil Kulkarni <anil@terminal.space>
2023-12-03 21:48:34 -0800

    fix tests

--- tests/test_pacman.py
+++ tests/test_pacman.py
@@ -6,10 +6,11 @@ import pytest
 from dfu.installed_packages.pacman import get_installed_packages
 
 
-def test_get_installed_packages_success():
-    with patch('subprocess.run') as mocked_run:
-        mocked_run.return_value = Mock()
-        mocked_run.return_value.stdout = '''
+@patch('subprocess.run')
+def test_get_installed_packages_success(mock_run):
+    mock_run.return_value = Mock()
+    mock_run.return_value.returncode = 0
+    mock_run.return_value.stdout = '''
 package1
 package3
 package2
@@ -17,16 +18,13 @@ package2
     leading_whitespace
     leading_and_trailing_whitespace    
 '''
-        mocked_run.return_value.returncode = 0
-        result = get_installed_packages()
-        assert result == ['leading_and_trailing_whitespace', 'leading_whitespace', 'package1', 'package2', 'package3']
+    result = get_installed_packages()
+    assert result == ['leading_and_trailing_whitespace', 'leading_whitespace', 'package1', 'package2', 'package3']
 
 
-def test_get_installed_packages_failure():
-    with patch('subprocess.run') as mocked_run:
-        mocked_run.return_value = Mock()
-        mocked_run.return_value.stdout = 'error\n'
-        mocked_run.return_value.returncode = 1
+@patch('subprocess.run')
+def test_get_installed_packages_failure(mock_run):
+    mock_run.side_effect = CalledProcessError(1, 'cmd')
 
-        with pytest.raises(CalledProcessError):
-            get_installed_packages()
+    with pytest.raises(CalledProcessError):
+        get_installed_packages()
