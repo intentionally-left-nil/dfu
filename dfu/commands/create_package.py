7f82b755eab56fae6659e63492c6ed544b87a906
Anil Kulkarni <anil@terminal.space>
2023-12-25 09:45:34 -0800

    Don't allow package names with ~, slashes, dots, etc

--- dfu/commands/create_package.py
+++ dfu/commands/create_package.py
@@ -6,7 +6,11 @@ from dfu.package.package_config import PackageConfig
 
 def create_package(config: Config, name: str, description: str | None = None) -> Path:
     package = PackageConfig(name=name, description=description)
-    package_name = Path(config.package_dir) / name / "dfu_config.json"
-    package_name.parent.mkdir(parents=True, exist_ok=False)
-    package.write(package_name)
-    return package_name
+    root_dir = Path(config.package_dir)
+    package_dir = root_dir / name
+    config_path = package_dir / 'dfu_config.json'
+    if package_dir.parent.resolve() != root_dir.resolve():
+        raise ValueError(f'Package name {name} is not allowed')
+    package_dir.mkdir(parents=True, exist_ok=False)
+    package.write(config_path)
+    return config_path
