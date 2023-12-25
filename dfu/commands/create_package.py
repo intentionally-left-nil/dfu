8a49091470f8d858cc13818fddd052182edcde2e
Anil Kulkarni <anil@terminal.space>
2023-12-25 09:31:16 -0800

    Remove numeric prefix from configs

--- dfu/commands/create_package.py
+++ dfu/commands/create_package.py
@@ -2,13 +2,11 @@ from pathlib import Path
 
 from dfu.config import Config
 from dfu.package.package_config import PackageConfig
-from dfu.package.version_number import get_version_number
 
 
 def create_package(config: Config, name: str, description: str | None = None) -> Path:
     package = PackageConfig(name=name, description=description)
-    version_number = str(get_version_number(config)).zfill(5)
-    package_name = Path(config.package_dir) / f"{version_number}_{name}" / "dfu_config.json"
+    package_name = Path(config.package_dir) / name / "dfu_config.json"
     package_name.parent.mkdir(parents=True, exist_ok=False)
     package.write(package_name)
     return package_name
