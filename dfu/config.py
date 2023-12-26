45c4a6047a83f04eee5f4d17edb80f1b0ad16d9e
Anil Kulkarni <anil@terminal.space>
2023-12-26 09:33:11 -0800

    Allow merging the config file from dropins and user locations. Also set the default package_dir to ~/.cache/dfu

--- dfu/config.py
+++ dfu/config.py
@@ -1,8 +1,10 @@
 import os
 import tomllib
 from dataclasses import dataclass
+from pathlib import Path
 
 from dataclass_wizard import fromdict
+from platformdirs import PlatformDirs
 
 
 @dataclass
@@ -13,7 +15,7 @@ class Btrfs:
 @dataclass
 class Config:
     btrfs: Btrfs
-    package_dir: str
+    package_dir: str | None = None
 
     @classmethod
     def from_file(cls, path: os.PathLike | str) -> "Config":
@@ -24,3 +26,6 @@ class Config:
     def from_toml(cls, toml: str) -> "Config":
         data = tomllib.loads(toml)
         return fromdict(cls, data)
+
+    def get_package_dir(self) -> Path:
+        return Path(self.package_dir) if self.package_dir else PlatformDirs("dfu").user_config_path / "packages"
