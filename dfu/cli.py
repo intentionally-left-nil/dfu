bbcc1053a5e9ecc9b58ab1a4edfb980bc8955106
Anil Kulkarni <anil@terminal.space>
2023-12-27 09:32:05 -0800

    simplify the commands imports

--- dfu/cli.py
+++ dfu/cli.py
@@ -4,13 +4,17 @@ from pathlib import Path
 
 import click
 
-from dfu.commands.create_config import create_config
-from dfu.commands.create_distribution import create_distribution
-from dfu.commands.create_package import create_package
-from dfu.commands.create_snapshot import create_post_snapshot, create_pre_snapshot
-from dfu.commands.diff import diff_snapshot
-from dfu.commands.load_config import get_config_paths, load_config
-from dfu.commands.load_package_config import get_package_path
+from dfu.commands import (
+    create_config,
+    create_distribution,
+    create_package,
+    create_post_snapshot,
+    create_pre_snapshot,
+    diff_snapshot,
+    get_config_paths,
+    get_package_path,
+    load_config,
+)
 from dfu.config import Config
 from dfu.snapshots.snapper import Snapper
 
