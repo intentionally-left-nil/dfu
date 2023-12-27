1b36940939dcb3d65dc1ce96be0a5c01a86569e9
Anil Kulkarni <anil@terminal.space>
2023-12-27 09:55:53 -0800

    Implement sorting in the config generator

--- dfu/commands/create_config.py
+++ dfu/commands/create_config.py
@@ -8,10 +8,19 @@ from tomlkit import dumps
 from dfu.config import Btrfs, Config
 from dfu.snapshots.btrfs import get_all_subvolumes
 from dfu.snapshots.snapper import Snapper
+from dfu.snapshots.sort_snapper_configs import sort_snapper_configs
 
 
 def create_config(file: Path, snapper_configs: list[str], package_dir: str | None):
     all_subvolumes = set(get_all_subvolumes())
+    all_snapper_configs = Snapper.get_configs()
+    unknown_configs = set(snapper_configs) - set([c.name for c in all_snapper_configs])
+    if unknown_configs:
+        click.echo(f"The following snapper configs were not found: {unknown_configs}", err=True)
+        raise ValueError("Unknown snapper configs")
+
+    snapper_configs = sort_snapper_configs([c for c in all_snapper_configs if c.name in snapper_configs])
+
     snapper_subvolumes = set([str(Snapper(c).get_mountpoint()) for c in snapper_configs])
     missing_subvolumes = all_subvolumes - snapper_subvolumes
     if missing_subvolumes:
