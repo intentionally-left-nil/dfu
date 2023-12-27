1c4589869cbe6d7b7a8a1511cdd1814323f5924a
Anil Kulkarni <anil@terminal.space>
2023-12-27 09:40:34 -0800

    Warn if snapper configs aren't present

--- dfu/commands/create_config.py
+++ dfu/commands/create_config.py
@@ -2,12 +2,21 @@ import subprocess
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
+import click
 from tomlkit import dumps
 
 from dfu.config import Btrfs, Config
+from dfu.snapshots.btrfs import get_all_subvolumes
+from dfu.snapshots.snapper import Snapper
 
 
 def create_config(file: Path, snapper_configs: list[str], package_dir: str | None):
+    all_subvolumes = set(get_all_subvolumes())
+    snapper_subvolumes = set([str(Snapper(c).get_mountpoint()) for c in snapper_configs])
+    missing_subvolumes = all_subvolumes - snapper_subvolumes
+    if missing_subvolumes:
+        click.echo(f"A snapper config was not found for the following subvolumes: {missing_subvolumes}", err=True)
+
     config = Config(btrfs=Btrfs(snapper_configs=snapper_configs), package_dir=package_dir)
     toml = dumps(config.__dict__)
     try:
