17f35b20fc93f9109e5453c0cae12edf984c4dc9
Anil Kulkarni <anil@terminal.space>
2023-12-27 13:01:25 -0800

    Change mkdir permissions to 755 everywhere

--- dfu/commands/create_distribution.py
+++ dfu/commands/create_distribution.py
@@ -8,6 +8,6 @@ def create_distribution(package_config_path: Path):
     package_config = PackageConfig.from_file(package_config_path)
     pkgbuild = to_pkgbuild(package_config)
     dist_folder = package_config_path.parent / 'dist'
-    dist_folder.mkdir(exist_ok=True)
+    dist_folder.mkdir(mode=0o755, exist_ok=True)
     with open(dist_folder / 'PKGBUILD', 'w') as f:
         f.write(pkgbuild)
--- dfu/commands/create_package.py
+++ dfu/commands/create_package.py
@@ -11,6 +11,6 @@ def create_package(config: Config, name: str, description: str | None = None) ->
     config_path = package_dir / 'dfu_config.json'
     if package_dir.parent.resolve() != root_dir.resolve():
         raise ValueError(f'Package name {name} is not allowed')
-    package_dir.mkdir(parents=True, exist_ok=False)
+    package_dir.mkdir(mode=0o755, parents=True, exist_ok=False)
     package.write(config_path)
     return config_path
