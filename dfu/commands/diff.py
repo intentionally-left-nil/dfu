7f34e5837787fb3fb12e99ab092a04261f64850c
Anil Kulkarni <anil@terminal.space>
2023-12-27 16:32:49 -0800

    Create placeholder files to denote which paths have changed

--- dfu/commands/diff.py
+++ dfu/commands/diff.py
@@ -3,11 +3,13 @@ from pathlib import Path
 from dfu.config import Config
 from dfu.installed_packages.pacman import diff_packages, get_installed_packages
 from dfu.package.package_config import PackageConfig
+from dfu.snapshots.snapper import Snapper
 
 
 def diff_snapshot(config: Config, package_config_path: Path):
     package_config = PackageConfig.from_file(package_config_path)
     update_installed_packages(config, package_config)
+    create_changed_placeholders(config, package_config)
     package_config.write(package_config_path)
 
 
@@ -20,3 +22,18 @@ def update_installed_packages(config: Config, package_config: PackageConfig):
     diff = diff_packages(old_packages, new_packages)
     package_config.programs_added = diff.added
     package_config.programs_removed = diff.removed
+
+
+def create_changed_placeholders(config: Config, package_config: PackageConfig):
+    pre_mapping = package_config.snapshot_mapping(use_pre_id=True)
+    post_mapping = package_config.snapshot_mapping(use_pre_id=False)
+
+    placeholder_dir = config.get_package_dir() / package_config.name / 'placeholders'
+    placeholder_dir.mkdir(mode=0o755, parents=True, exist_ok=True)
+    for snapper_name, pre_id in pre_mapping.items():
+        post_id = post_mapping[snapper_name]
+        snapper = Snapper(snapper_name)
+        for change in snapper.get_delta(pre_id, post_id):
+            path = placeholder_dir / change.path.lstrip('/')
+            path.parent.mkdir(mode=0o755, parents=True, exist_ok=True)
+            path.write_text(f"PLACEHOLDER: {change.action}")
