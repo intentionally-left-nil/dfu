305af0524fbf2a6c5f4d7cfa0fe33b966d75c15a
Anil Kulkarni <anil@terminal.space>
2023-12-29 08:33:22 -0800

    Try using mkdir as the default path to see if it speeds things up

--- dfu/commands/diff.py
+++ dfu/commands/diff.py
@@ -42,19 +42,22 @@ def create_changed_placeholders(package_config: PackageConfig, package_dir: Path
             if not path.resolve().is_relative_to(placeholder_dir.resolve()):
                 raise ValueError(f"Trying to create {path} failed because it is not relative to {placeholder_dir}")
 
-            # We can't simply make all the parent directories, because the snapper diff doesn't distinguish between files and directories.
-            # Therefore, we may have previously created a parent directory as a file. So, we need to manually walk the path
-            # Delete any placeholder files that are actually directories, and re-create them as directories
-            current_path = Path(path.parts[0])
-            for child in path.parts[1:-1]:
-                current_path = current_path / child
+            try:
+                path.parent.mkdir(parents=True, exist_ok=True, mode=0o755)
+            except FileExistsError:
+                # We can't simply make all the parent directories, because the snapper diff doesn't distinguish between files and directories.
+                # Therefore, we may have previously created a parent directory as a file. So, we need to manually walk the path
+                # Delete any placeholder files that are actually directories, and re-create them as directories
+                current_path = Path(path.parts[0])
+                for child in path.parts[1:-1]:
+                    current_path = current_path / child
 
-                if current_path.is_file() and current_path.read_text().startswith("PLACEHOLDER"):
-                    current_path.unlink()
-                    current_path.mkdir(mode=0o755)
-                elif not current_path.exists():
-                    current_path.mkdir(mode=0o755)
-                elif not current_path.is_dir():
-                    raise ValueError(f"Trying to create {path} failed because {current_path} is not a directory")
+                    if current_path.is_file():
+                        current_path.unlink()
+                        current_path.mkdir(mode=0o755)
+                    elif not current_path.exists():
+                        current_path.mkdir(mode=0o755)
+                    elif not current_path.is_dir():
+                        raise ValueError(f"Trying to create {path} failed because {current_path} is not a directory")
 
             path.write_text(f"PLACEHOLDER: {change.action}\n")
