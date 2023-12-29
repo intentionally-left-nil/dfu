c10b4aae9464d9582d2bcc9bd41940014a9e4bda
Anil Kulkarni <anil@terminal.space>
2023-12-29 09:53:37 -0800

    Add code comments

--- dfu/commands/diff.py
+++ dfu/commands/diff.py
@@ -27,6 +27,7 @@ def update_installed_packages(config: Config, package_config: PackageConfig):
 
 
 def create_changed_placeholders(package_config: PackageConfig, package_dir: Path):
+    # This method has been performance optimized in several places. Take care when modifying the file for both correctness and speed
     pre_mapping = package_config.snapshot_mapping(use_pre_id=True)
     post_mapping = package_config.snapshot_mapping(use_pre_id=False)
 
@@ -40,20 +41,29 @@ def create_changed_placeholders(package_config: PackageConfig, package_dir: Path
         snapper = Snapper(snapper_name)
         deltas = snapper.get_delta(pre_id, post_id)
 
+        # Cumulatively, it's expensive to create files, so we want to filter out .gitignore files and skip writing them
+        # git_check_ignore will return a list of all the paths that are ignored by git, but we need to be careful about the path
+        # As far as git is concerned, the path should be placeholders/<path>, so we will set that once here
+        # And then the code to actually write the file joins it with package_dir later
         for delta in deltas:
             delta.path = f"placeholders/{delta.path.lstrip('/')}"
 
+        # Create a set of all the ignored files. Earlier attempts tried using a list and checking the last element, but the ordering wasn't exact
         ignored_paths = set(git_check_ignore(package_dir, [delta.path for delta in deltas]))
         for delta in deltas:
             if delta.path in ignored_paths:
+                # Performance speedup: Don't write files that are ignored by git
                 continue
             path = package_dir / delta.path
             try:
+                # Performance speedup: Try calling mkdir once, to create all of the parent directories
                 path.parent.mkdir(parents=True, exist_ok=True, mode=0o755)
             except FileExistsError:
-                # We can't simply make all the parent directories, because the snapper diff doesn't distinguish between files and directories.
+                # Calling mkdir() doesn't work in all cases, because sometimes we mistakenly create a file, instead of a directory
+                # diff_packages doesn't distinguish between files and directories, so the placeholder algorithm assumes they're always files
                 # Therefore, we may have previously created a parent directory as a file. So, we need to manually walk the path
                 # Delete any placeholder files that are actually directories, and re-create them as directories
+                # This is the slow path, so only do it if there are conflicts
                 current_path = Path(path.parts[0])
                 for child in path.parts[1:-1]:
                     current_path = current_path / child
