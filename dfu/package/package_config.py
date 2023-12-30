49c05b605db33a64d9f067044b7585160f9a5fab
Anil Kulkarni <anil@terminal.space>
2023-12-30 09:57:14 -0800

    Refactor json serialization code into a helper mixin

--- dfu/package/package_config.py
+++ dfu/package/package_config.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 from dataclass_wizard import asdict, fromdict
 
+from dfu.helpers.json_serializable import JsonSerializableMixin
+
 
 @dataclass
 class Snapshot:
@@ -16,17 +18,13 @@ SnapshotMapping = dict[str, int]
 
 
 @dataclass
-class PackageConfig:
+class PackageConfig(JsonSerializableMixin):
     name: str
     description: str | None
     snapshots: list[dict[str, Snapshot]] = field(default_factory=list)
     programs_added: list[str] = field(default_factory=list)
     programs_removed: list[str] = field(default_factory=list)
     version: str = "0.0.1"
-
-    def write(self, path: os.PathLike | str):
-        with open(path, "w") as f:
-            json.dump(asdict(self), f, indent=4)
 
     def snapshot_mapping(self, *, use_pre_id: bool, index: int = -1) -> SnapshotMapping:
         snapshot = self.snapshots[index]
@@ -40,15 +38,6 @@ class PackageConfig:
                     raise ValueError("Post-snapshot does not exist")
                 mapping[k] = v.post_id
         return mapping
-
-    @classmethod
-    def from_file(cls, path: os.PathLike | str) -> "PackageConfig":
-        with open(path) as f:
-            return cls.from_json(f.read())
-
-    @classmethod
-    def from_json(cls, data: str) -> "PackageConfig":
-        return fromdict(cls, json.loads(data))
 
 
 def find_package_config(path: Path) -> Path | None:
--- tests/test_package_config.py
+++ tests/test_package_config.py
@@ -1,10 +1,8 @@
 import json
-import tempfile
 from dataclasses import dataclass, field
 from pathlib import Path
 from unittest.mock import patch
 
-import dataclass_wizard.errors
 import pytest
 
 from dfu.package.package_config import PackageConfig, Snapshot, find_package_config
@@ -58,32 +56,6 @@ def test_valid_configs(test: ValidConfigTest):
     ]
     expected = PackageConfig(name=test.name, description=test.description, snapshots=expected_snapshots)
     assert actual == expected
-
-
-def test_from_file():
-    json_data = '{"name": "expected_name", "description": "expected_description"}'
-    with tempfile.NamedTemporaryFile("w") as f:
-        f.write(json_data)
-        f.flush()
-        actual = PackageConfig.from_file(f.name)
-        expected = PackageConfig(name='expected_name', description='expected_description')
-        assert actual == expected
-
-
-def test_missing_field():
-    invalid_json_data = '{"name": "expected_name"}'
-    with pytest.raises(dataclass_wizard.errors.MissingFields):
-        PackageConfig.from_json(invalid_json_data)
-
-
-def test_write_config():
-    config = PackageConfig(name='expected_name', description='expected_description')
-    with tempfile.NamedTemporaryFile("w") as f:
-        config.write(f.name)
-        f.flush()
-        actual = PackageConfig.from_file(f.name)
-        expected = PackageConfig(name='expected_name', description='expected_description')
-        assert actual == expected
 
 
 @pytest.fixture
