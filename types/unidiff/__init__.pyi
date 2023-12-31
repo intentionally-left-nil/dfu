b4fe76b138578928ce9cc5277e2e8912b6f13b0a
Anil Kulkarni <anil@terminal.space>
2023-12-31 12:12:59 -0800

    Add unidiff as a dependency, and generate the type stubs

---
+++ types/unidiff/__init__.pyi
@@ -0,0 +1,5 @@
+from _typeshed import Incomplete
+from unidiff import __version__ as __version__
+from unidiff.patch import DEFAULT_ENCODING as DEFAULT_ENCODING, Hunk as Hunk, LINE_TYPE_ADDED as LINE_TYPE_ADDED, LINE_TYPE_CONTEXT as LINE_TYPE_CONTEXT, LINE_TYPE_REMOVED as LINE_TYPE_REMOVED, PatchSet as PatchSet, PatchedFile as PatchedFile, UnidiffParseError as UnidiffParseError
+
+VERSION: Incomplete
