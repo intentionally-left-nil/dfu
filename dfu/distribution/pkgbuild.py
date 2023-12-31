7b6bfd28a5d51274acfd5e5e1a36d7096f00157b
Anil Kulkarni <anil@terminal.space>
2023-12-31 01:11:22 -0800

    Add the patch file to the source() array of PKGBUILD

--- dfu/distribution/pkgbuild.py
+++ dfu/distribution/pkgbuild.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from dfu.package.package_config import PackageConfig
 
 PKGBUILD_TEMPLATE = """\
@@ -8,13 +10,21 @@ pkgdesc='{description}'
 arch=('any')
 license=('MIT')
 depends=({dependencies})
+source=({sources})
+
+sha256sums=({checksums})
 """
 
 
-def to_pkgbuild(package_config: PackageConfig) -> str:
+def to_pkgbuild(package_config: PackageConfig, patch: str | None) -> str:
+    sources = ["changes.patch"] if patch else []
+    checksums = [hashlib.sha256(patch.encode('utf-8')).hexdigest()] if patch else []
+
     return PKGBUILD_TEMPLATE.format(
         name=package_config.name,
         version=package_config.version,
         description=package_config.description,
         dependencies=" ".join(package_config.programs_added),
+        sources=" ".join(sources),
+        checksums=" ".join(checksums),
     )
--- tests/test_pkgbuild.py
+++ tests/test_pkgbuild.py
@@ -4,11 +4,11 @@ from dfu.distribution.pkgbuild import to_pkgbuild
 from dfu.package.package_config import PackageConfig
 
 
-def test_to_pkgbuild():
+def test_to_pkgbuild_no_patch_file():
     package_config = PackageConfig(
         name="test", description="my cool description", programs_added=["test1", "test2"], version="0.0.2"
     )
-    actual = to_pkgbuild(package_config)
+    actual = to_pkgbuild(package_config, patch=None)
     expected = """\
 pkgname='test'
 pkgver='0.0.2'
@@ -17,5 +17,36 @@ pkgdesc='my cool description'
 arch=('any')
 license=('MIT')
 depends=(test1 test2)
+source=()
+
+sha256sums=()
+"""
+    assert actual == expected
+
+
+def test_to_pkgbuild():
+    package_config = PackageConfig(
+        name="test", description="my cool description", programs_added=["test1", "test2"], version="0.0.2"
+    )
+    patch = """\
+diff --git a/files/etc/myfile b/files/etc/myfile
+index 51745d0..b6b08c0 100644
+--- a/files/etc/myfile
++++ b/files/etc/myfile
+@@ -1 +1 @@
++hello
+"""
+    actual = to_pkgbuild(package_config, patch=patch)
+    expected = """\
+pkgname='test'
+pkgver='0.0.2'
+pkgrel=1
+pkgdesc='my cool description'
+arch=('any')
+license=('MIT')
+depends=(test1 test2)
+source=(changes.patch)
+
+sha256sums=(1a5c5561fd4de8b44f96054b773eb1ddd08a6fbf41a5953f9021b2369469a942)
 """
     assert actual == expected
