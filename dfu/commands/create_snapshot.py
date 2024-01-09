6eab8b64bc0674161af3ebc238dd336c92219560
Anil Kulkarni <anil@terminal.space>
2024-01-08 16:57:39 -0800

    Update create_snapshot to use the new package_config data type

--- dfu/commands/create_snapshot.py
+++ dfu/commands/create_snapshot.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 
 from dfu.config import Config
-from dfu.package.package_config import PackageConfig, SnapshotDeprecated
+from dfu.package.package_config import PackageConfig
 from dfu.snapshots.snapper import Snapper
 
 
 def create_snapshot(config: Config, package_dir: Path):
     package_config = PackageConfig.from_file(package_dir / "dfu_config.json")
-    snapshots_deprecated: dict[str, SnapshotDeprecated] = {}
+    snapshot: dict[str, int] = {}
     for snapper_config in config.btrfs.snapper_configs:
         snapper = Snapper(snapper_config)
-        pre_id = snapper.create_snapshot(package_config.description or package_config.name)
-        snapshots_deprecated[snapper_config] = SnapshotDeprecated(pre_id=pre_id)
-    package_config.snapshots_deprecated.append(snapshots_deprecated)
+        snapshot_id = snapper.create_snapshot(package_config.description or package_config.name)
+        snapshot[snapper_config] = snapshot_id
+    package_config.snapshots.append(snapshot)
     package_config.write(package_dir / "dfu_config.json")
