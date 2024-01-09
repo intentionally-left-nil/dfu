9d2418cf84fae7f579c145d6b5c40f91e606eead
Anil Kulkarni <anil@terminal.space>
2024-01-09 11:36:35 -0800

    Simplify the update method to just use kwargs

--- dfu/api/state.py
+++ dfu/api/state.py
@@ -1,20 +1,8 @@
 from dataclasses import dataclass, replace
 from pathlib import Path
-from typing import TypeVar
 
 from dfu.config import Config
 from dfu.package.package_config import PackageConfig
-
-
-class Sentinel:
-    pass
-
-
-SENTINEL = Sentinel()
-
-
-T = TypeVar('T')
-Replacement = T | Sentinel | None
 
 
 @dataclass(frozen=True)
@@ -23,12 +11,5 @@ class State:
     package_dir: Path | None = None
     package_config: PackageConfig | None = None
 
-    def update(
-        self,
-        config: Replacement[Config],
-        package_dir: Replacement[Path] = SENTINEL,
-        package_config: Replacement[PackageConfig] = SENTINEL,
-    ) -> 'State':
-        kwargs = {"config": config, "package_dir": package_dir, "package_config": package_config}
-        kwargs = {k: v for k, v in kwargs.items() if v is not SENTINEL}
+    def update(self, **kwargs) -> 'State':
         return replace(self, **kwargs)
