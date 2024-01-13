ac5a1a8b7dd2612afcd3207d7e11b57c223f2c60
Anil Kulkarni <anil@terminal.space>
2024-01-13 14:52:07 -0800

    ignore expected mypy error when checking error test conditions

--- tests/test_state.py
+++ tests/test_state.py
@@ -11,7 +11,7 @@ from dfu.package.package_config import PackageConfig
 def test_state_is_immutable(config: Config, package_config: PackageConfig):
     state = State(config=config, package_dir=Path("test"), package_config=package_config)
     with pytest.raises(FrozenInstanceError):
-        state.package_dir = Path("test2")
+        state.package_dir = Path("test2")  # type: ignore
 
 
 def test_update_state(config: Config, package_config: PackageConfig):
