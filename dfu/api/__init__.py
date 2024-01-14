9329bf7de908fc8acfdb43030744400c23eb48c8
Anil Kulkarni <anil@terminal.space>
2024-01-14 09:57:24 -0800

    Create the autosave plugin to ensure the file state and the in-memory state remain in-sync

--- dfu/api/__init__.py
+++ dfu/api/__init__.py
@@ -1,4 +1,4 @@
 from dfu.api.entrypoint import Entrypoint
 from dfu.api.plugin import DfuPlugin, Event
 from dfu.api.state import State
-from dfu.api.store import Store
+from dfu.api.store import Callback, Store
