a8b3684e0316c4761e9bc46180acfb08db722e7f
Anil Kulkarni <anil@terminal.space>
2024-01-13 19:25:45 -0800

    Save the store, and add an event enum

--- dfu/api/plugin.py
+++ dfu/api/plugin.py
@@ -1,7 +1,13 @@
 from abc import ABC, abstractmethod
+from enum import StrEnum, auto
+from typing import Literal
+
+
+class Event(StrEnum):
+    TARGET_BRANCH_FINALIZED = auto()
 
 
 class DfuPlugin(ABC):
     @abstractmethod
-    def handle(self):
+    def handle(self, event: Event):
         pass
--- dfu/plugins/pacman.py
+++ dfu/plugins/pacman.py
@@ -1,10 +1,17 @@
 from dfu.api.entrypoint import Entrypoint
-from dfu.api.plugin import DfuPlugin
+from dfu.api.plugin import DfuPlugin, Event
+from dfu.api.store import Store
 
 
 class PacmanPlugin(DfuPlugin):
-    def handle(self):
-        pass
+    store: Store
+
+    def __init__(self, store: Store):
+        self.store = store
+
+    def handle(self, event: Event):
+        if event == Event.TARGET_BRANCH_FINALIZED:
+            print("PacmanPlugin: Target branch finalized")
 
 
-entrypoint: Entrypoint = lambda store: PacmanPlugin()
+entrypoint: Entrypoint = lambda store: PacmanPlugin(store)
