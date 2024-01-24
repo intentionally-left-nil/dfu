779b74b2bbffafd3b26661184c73b39bcd57b8a9
Anil Kulkarni <anil@terminal.space>
2024-01-23 19:56:58 -0800

    Add uninstall cli entrypoint

--- dfu/commands/__init__.py
+++ dfu/commands/__init__.py
@@ -6,3 +6,4 @@ from dfu.commands.diff import abort_diff, begin_diff, continue_diff
 from dfu.commands.install import abort_install, begin_install, continue_install
 from dfu.commands.load_config import get_config_paths, load_config
 from dfu.commands.load_store import load_store
+from dfu.commands.uninstall import abort_uninstall, begin_uninstall, continue_uninstall
---
+++ dfu/commands/uninstall.py
@@ -0,0 +1,13 @@
+from dfu.api import Store
+
+
+def begin_uninstall(store: Store):
+    pass
+
+
+def continue_uninstall(store: Store):
+    pass
+
+
+def abort_uninstall(store: Store):
+    pass
