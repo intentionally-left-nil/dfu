ac1269b72a2fbfc63d70860ee7104a5d16c611fe
Anil Kulkarni <akulkarni@anaconda.com>
2024-01-29 07:51:07 -0800

    Refactor files_modified to return per-mountpoint information

--- dfu/commands/ls_files.py
+++ dfu/commands/ls_files.py
@@ -10,5 +10,6 @@ def ls_files(store: Store, *, from_index: int, to_index: int, only_ignored: bool
     to_index = normalize_snapshot_index(store.state.package_config, to_index)
     if from_index > to_index:
         raise ValueError(f"from_index {from_index} is greater than to_index {to_index}")
-    for file in files_modified(store, from_index=from_index, to_index=to_index, only_ignored=only_ignored):
-        click.echo(file)
+    for files in files_modified(store, from_index=from_index, to_index=to_index, only_ignored=only_ignored).values():
+        for file in files:
+            click.echo(file)
