8cd85052b87ac3f2311e7bbe997373d13b7a9382
Anil Kulkarni <akulkarni@anaconda.com>
2024-01-29 14:37:01 -0800

    type error

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -6,7 +6,7 @@ from dfu.api import DfuPlugin, Event, Store
 
 def test_plugin():
     class TestPlugin(DfuPlugin):
-        def handle(self, event: Event) -> Any:
+        def handle(self, event: Event, **kwargs) -> Any:
             pass
 
     def entrypoint(store: Store) -> DfuPlugin:
